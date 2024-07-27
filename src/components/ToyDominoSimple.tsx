b25bd125ff7b40acfff06715cbb0a99d9b4aece3
naotaro0123 <n.ikewada@gmail.com>
2024-07-27 16:33:20 +0900

    fix: add start ball

--- src/components/ToyDominoSimple.tsx
+++ src/components/ToyDominoSimple.tsx
@@ -1,11 +1,14 @@
-import { Physics, RigidBody } from '@react-three/rapier';
+import { Sphere } from '@react-three/drei';
+import { Physics, RapierRigidBody, RigidBody } from '@react-three/rapier';
 import { button, useControls } from 'leva';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const dominoHeight = 1.4;
 const dominoPosY = 0;
 const floorOffset = 0.05;
 const floorPosY = -dominoHeight / 2 - floorOffset;
+const ballPosY = -0.5;
+const ballPosZ = -4;
 
 type DominoType = {
   id: string;
@@ -17,10 +20,42 @@ type DominoType = {
 const createDominoMesh = (domino: DominoType) => {
   const { id, position, rotation, color } = domino;
   return (
-    <mesh key={id} name={id} position={position} rotation={rotation}>
-      <boxGeometry args={[0.4, dominoHeight, 0.2]} />
-      <meshStandardMaterial color={color} />
-    </mesh>
+    <RigidBody colliders="cuboid">
+      <mesh key={id} name={id} position={position} rotation={rotation} castShadow>
+        <boxGeometry args={[0.4, dominoHeight, 0.2]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    </RigidBody>
+  );
+};
+
+const createBallMesh = (isStart: boolean) => {
+  const rb = useRef<RapierRigidBody>(null);
+
+  useEffect(() => {
+    if (isStart && rb.current) {
+      rb.current.setTranslation({ x: 0, y: ballPosY, z: ballPosZ }, true);
+      rb.current.setLinvel({ x: 0, y: 0, z: 2 }, true);
+    }
+  });
+
+  return (
+    <RigidBody ref={rb} colliders="ball" position={[0, ballPosY, ballPosZ]}>
+      <Sphere args={[0.4]} castShadow>
+        <meshStandardMaterial color={'red'} />
+      </Sphere>
+    </RigidBody>
+  );
+};
+
+const createFloorMesh = () => {
+  return (
+    <RigidBody type="fixed" colliders="cuboid" position={[0, floorPosY, 0]}>
+      <mesh castShadow>
+        <boxGeometry args={[10, 0.1, 10]} />
+        <meshStandardMaterial color={'grey'} />
+      </mesh>
+    </RigidBody>
   );
 };
 
@@ -41,15 +76,11 @@ const dominoSettings: DominoType[] = [
 export const ToyDominoSimple = () => {
   const [debug, setDebug] = useState<boolean>(true);
   const [isStart, setIsStart] = useState<boolean>(false);
-  const [isReset, setIsReset] = useState<boolean>(false);
 
   useControls({
     debugMode: { value: debug, onChange: setDebug },
     start: button(() => setIsStart(true)),
-    reset: button(() => {
-      setIsReset(true);
-      setIsStart(false);
-    }),
+    reset: button(() => window.location.reload()),
   });
 
   return (
@@ -59,17 +90,9 @@ export const ToyDominoSimple = () => {
         <directionalLight position={[-5, -5, -3]} intensity={2} />
       </group>
       <Physics debug={debug} colliders={false}>
-        {/* Floor */}
-        <RigidBody type="fixed" colliders="cuboid" position={[0, floorPosY, 0]}>
-          <mesh>
-            <boxGeometry args={[10, 0.1, 10]} />
-            <meshStandardMaterial color={'grey'} />
-          </mesh>
-        </RigidBody>
-
-        {dominoSettings.map((domino) => (
-          <RigidBody colliders="cuboid">{createDominoMesh(domino)}</RigidBody>
-        ))}
+        {createBallMesh(isStart)}
+        {createFloorMesh()}
+        {dominoSettings.map((domino) => createDominoMesh(domino))}
       </Physics>
     </>
   );
