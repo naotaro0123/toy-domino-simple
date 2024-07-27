1976b394d339e9ef9cd584e3cfc4f4e58d71db3a
naotaro0123 <n.ikewada@gmail.com>
2024-07-27 15:05:23 +0900

    fix: setup floor and color

--- src/components/ToyDominoSimple.tsx
+++ src/components/ToyDominoSimple.tsx
@@ -1,32 +1,64 @@
-type Domino = { id: string; position: [number, number, number]; color: string };
-const createDomino = (domino: Domino) => {
+import { Physics, RigidBody } from '@react-three/rapier';
+import { useState } from 'react';
+
+const dominoHeight = 1.4;
+const dominoPosY = 0;
+const floorOffset = 0.05;
+const floorPosY = -dominoHeight / 2 - floorOffset;
+
+type DominoType = {
+  id: string;
+  position: [number, number, number];
+  rotation?: [number, number, number];
+  color: string;
+};
+
+const createDominoMesh = (domino: DominoType) => {
+  const { id, position, rotation, color } = domino;
   return (
-    <mesh key={domino.id} name={domino.id} position={domino.position}>
-      <boxGeometry args={[0.4, 2, 0.2]} />
-      <meshStandardMaterial color={domino.color} />
+    <mesh key={id} name={id} position={position} rotation={rotation}>
+      <boxGeometry args={[0.4, dominoHeight, 0.2]} />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };
 
-const dominoSettings: Domino[] = [
-  { id: '1', position: [1, 0, 4], color: 'red' },
-  { id: '2', position: [2, 0, 3], color: 'blue' },
-  { id: '3', position: [3, 0, 2], color: 'green' },
-  { id: '4', position: [4, 0, 1], color: 'yellow' },
+const randomColor = (): string => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r},${g},${b})`;
+};
+
+const dominoSettings: DominoType[] = [
+  { id: '1', position: [0, dominoPosY, 1], color: randomColor() },
+  { id: '2', position: [0, dominoPosY, 2], color: randomColor() },
+  { id: '3', position: [0, dominoPosY, 3], color: randomColor() },
+  { id: '4', position: [0, dominoPosY, 4], color: randomColor() },
 ];
 
 export const ToyDominoSimple = () => {
+  const [debug, setDebug] = useState<boolean>(true);
+
   return (
     <>
       <group>
         <directionalLight position={[5, 5, 3]} intensity={2} />
         <directionalLight position={[-5, -5, -3]} intensity={2} />
       </group>
-      <mesh name="floor">
-        <boxGeometry args={[10, 0.1, 10]} />
-        <meshStandardMaterial color={'grey'} />
-      </mesh>
-      {dominoSettings.map((domino) => createDomino(domino))}
+      <Physics debug={debug} colliders={false}>
+        {/* Floor */}
+        <RigidBody type="fixed" colliders="cuboid" position={[0, floorPosY, 0]}>
+          <mesh>
+            <boxGeometry args={[10, 0.1, 10]} />
+            <meshStandardMaterial color={'grey'} />
+          </mesh>
+        </RigidBody>
+
+        {dominoSettings.map((domino) => (
+          <RigidBody colliders="cuboid">{createDominoMesh(domino)}</RigidBody>
+        ))}
+      </Physics>
     </>
   );
 };
