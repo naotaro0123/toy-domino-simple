b9b4321122f151c46cf287be5c23ed7f63a7d030
naotaro0123 <n.ikewada@gmail.com>
2024-08-04 15:12:42 +0900

    fix: fixed mesh top color

--- src/components/Domino.tsx
+++ src/components/Domino.tsx
@@ -9,13 +9,24 @@ export type DominoType = {
 export const dominoPosY = 0;
 export const dominoHeight = 1.4;
 
+const meshTopIndex = 2;
+const meshBottomIndex = 3;
+const meshFixedColor = 'white';
+
 export const createDominoMesh = (domino: DominoType, i: number) => {
   const { position, rotation, color } = domino;
   return (
     <RigidBody colliders="cuboid" key={i}>
       <mesh position={position} rotation={rotation} castShadow>
         <boxGeometry args={[0.6, dominoHeight, 0.2]} />
-        <meshStandardMaterial color={color} />
+
+        {Array.from({ length: 6 }).map((_, i) => (
+          <meshStandardMaterial
+            key={i}
+            attach={`material-${i}`}
+            color={i === meshTopIndex || i === meshBottomIndex ? meshFixedColor : color}
+          />
+        ))}
       </mesh>
     </RigidBody>
   );
