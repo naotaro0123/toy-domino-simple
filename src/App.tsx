2d7e7a337b25eef7adef15546bbff4342a322e4d
naotaro0123 <n.ikewada@gmail.com>
2024-08-04 17:44:18 +0900

    refactor: move useControls to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, OrthographicCamera, PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { button, useControls } from 'leva';
+import { useState } from 'react';
 import './App.css';
 import { ToyDominoSimple } from './components/ToyDominoSimple';
 
+const stages = [0, 1] as const;
+type StageType = (typeof stages)[number];
+
 function App() {
+  const [debug, setDebug] = useState<boolean>(true);
+  const [isStart, setIsStart] = useState<boolean>(false);
+  const [stage, setStage] = useState<StageType>(0);
+
+  const changeStage = (stage: StageType) => {
+    setStage(stage);
+    setIsStart(false);
+  };
+
+  useControls({
+    debugMode: { value: debug, onChange: setDebug },
+    stage: { value: stage, options: stages, onChange: changeStage },
+    start: button(() => setIsStart(true)),
+    reset: button(() => window.location.reload()),
+  });
+
   return (
     <>
-      {/* <Canvas camera={{ position: [6, 6, 12], zoom: 2.2 }}> */}
-      {/* TopView */}
-      <Canvas orthographic camera={{ position: [0, 14, 0], zoom: 30 }}>
+      <Canvas>
+        <PerspectiveCamera position={[6, 6, 12]} zoom={2.2} makeDefault={stage === 0} />
+        <OrthographicCamera position={[0, 14, 0]} zoom={38} makeDefault={stage === 1} />
         <color attach="background" args={['lightgray']} />
         <OrbitControls />
-        <ToyDominoSimple />
+        <ToyDominoSimple debug={debug} isStart={isStart} stage={stage} />
       </Canvas>
     </>
   );
--- src/components/ToyDominoSimple.tsx
+++ src/components/ToyDominoSimple.tsx
@@ -1,26 +1,17 @@
 import { Physics } from '@react-three/rapier';
-import { button, useControls } from 'leva';
-import { useState } from 'react';
 import { dominoSettings } from '../utils/dominoSettings';
 import { createBallMesh } from './Ball';
 import { createDominoMesh } from './Domino';
 import { createFloorMesh } from './Floor';
 
-const stages = [0, 1] as const;
-type StageType = (typeof stages)[number];
+type ToyDominoProps = {
+  debug: boolean;
+  isStart: boolean;
+  stage: number;
+};
 
-export const ToyDominoSimple = () => {
-  const [debug, setDebug] = useState<boolean>(true);
-  const [isStart, setIsStart] = useState<boolean>(false);
-  const [stage, setStage] = useState<StageType>(1);
-
-  useControls({
-    debugMode: { value: debug, onChange: setDebug },
-    stage: { value: stage, options: stages, onChange: setStage },
-    start: button(() => setIsStart(true)),
-    reset: button(() => window.location.reload()),
-  });
-
+export const ToyDominoSimple = (props: ToyDominoProps) => {
+  const { debug, isStart, stage } = props;
   return (
     <>
       <group>
