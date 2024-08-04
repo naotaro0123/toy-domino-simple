789c52d8fcb35e783e0f45081db064184484baa4
naotaro0123 <n.ikewada@gmail.com>
2024-08-04 17:48:25 +0900

    fix: dominos position

--- src/utils/stage0.ts
+++ src/utils/stage0.ts
@@ -2,11 +2,11 @@ import { dominoHeight, dominoPosY, DominoType } from '../components/Domino';
 import { randomColor } from './color';
 
 export const stage0: DominoType[] = [
-  { position: [0, dominoPosY, 3.0], color: randomColor() },
-  { position: [0, dominoPosY, 2.2], color: randomColor() },
-  { position: [0, dominoPosY, 1.4], color: randomColor() },
+  { position: [0, dominoPosY, 3.2], color: randomColor() },
+  { position: [0, dominoPosY, 2.4], color: randomColor() },
+  { position: [0, dominoPosY, 1.5], color: randomColor() },
   { position: [0, dominoPosY, 0.6], color: randomColor() },
-  { position: [0, dominoPosY, -0.2], color: randomColor() },
+  { position: [0, dominoPosY, -0.3], color: randomColor() },
   { position: [0, dominoPosY, -1.0], color: randomColor() },
   { position: [0.2, dominoPosY, -1.8], rotation: [0, -Math.PI / 4, 0], color: randomColor() },
   { position: [0.8, dominoPosY, -2.2], rotation: [0, -Math.PI / 2, 0], color: randomColor() },
